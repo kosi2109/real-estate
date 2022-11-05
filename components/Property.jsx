2d5706ecea39c0a67ef3dcf6034c284bf22020ab
Si Thu Htet <nolockfacebook55@gmail.com>
2022-11-05 21:11:01 +0630

    fix ui

--- components/Property.jsx
+++ components/Property.jsx
@@ -10,8 +10,8 @@ import DefaultImage from '../assets/images/house.jpg';
 
 const Property = ({ property: { coverPhoto, price, rentFrequency, rooms, title, baths, area, agency, isVerified, externalID  } }) => (
   <Link href={`/property/${externalID}`} passHref>
-    <Flex flexWrap='wrap' maxW='420px' p='5' paddingTop='0px' justifyContent='flex-start' cursor='pointer' borderRadius="2px" >
-      <Box w={400} h={260} overflow="hidden">
+    <Flex flexWrap='wrap' maxW='420px' p='5' paddingTop='0px' justifyContent='flex-start' cursor='pointer' >
+      <Box w="100%" h={260} overflow="hidden">
         <Image src={coverPhoto ? coverPhoto.url : DefaultImage} width={400} height={260} />
       </Box>
       <Box w='full'>
